291d77244df9184f964144412395ade1107af5c2
zack <zack_apollo_t0817@ie8.com>
2023-07-11 02:17:07 +0800

    清除指令

--- app/Http/Controllers/Customers/TestController.php
+++ app/Http/Controllers/Customers/TestController.php
@@ -7,22 +7,32 @@ use App\Models\Customer;
 use App\Models\SocialAccount;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
+
+
 
 class TestController extends Controller
 {
 
     public function axcd(Request $request)
     {
-        $type = $request->get('type');
-        switch ($type) {
-            case 'clear_account':
-                SocialAccount::truncate();
-                DB::table('customers')->delete(); 
-                DB::table('otps')->delete();                  
-                return Response::success();
-                break;
-            default:
-                break;
-        }
+        DB::table('coupon_codes')->delete();
+        DB::table('coupon_customer_redeems')->delete();
+        DB::table('coupon_customers')->delete();
+        DB::table('coupons')->delete();
+        DB::table('customers')->delete();
+        DB::table('invoices')->delete();
+        DB::table('login_customers')->delete();
+        DB::table('otps')->delete();
+        DB::table('personal_access_tokens')->delete();
+        DB::table('point_customers')->delete();
+        DB::table('social_accounts')->delete();
+        DB::table('stamp_customers')->delete();
+        DB::table('store_employees')->delete();
+        DB::table('store_privilege_intermediates')->delete();
+        DB::table('store_privilege_menus')->delete();
+        DB::table('store_privilege_roles')->delete();
+        DB::table('stores')->delete();
+        Artisan::call('db:seed');
     }
 }
