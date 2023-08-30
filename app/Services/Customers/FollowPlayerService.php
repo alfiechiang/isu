960bd4c827665df430f6af0fb7eeab8b5e8e90f8
zack <zack_apollo_t0817@ie8.com>
2023-08-30 16:20:01 +0800

    update

--- app/Services/Customers/FollowPlayerService.php
+++ app/Services/Customers/FollowPlayerService.php
@@ -12,7 +12,7 @@ class FollowPlayerService
     {
 
         $Builder = new FollowPlayer();
-        return $Builder->whereIn('area', $data['region'])->where('review', true)->get();
+        return $Builder->where('area', 'LIKE', '%'.$data['region'].'%')->where('review', true)->get();
     }
 
     public function findone($follow_id)
@@ -22,20 +22,13 @@ class FollowPlayerService
 
     public function stronghold(){
 
-        $northAreas = County::where('region', 'north')->pluck('cn_name')->toArray();
-        $middleAreas = County::where('region', 'middle')->pluck('cn_name')->toArray();
-        $southAreas = County::where('region', 'south')->pluck('cn_name')->toArray();
-        $EastAreas = County::where('region', 'east')->pluck('cn_name')->toArray();
-        $OutIslandAreas = County::where('region', 'out_island')->pluck('cn_name')->toArray();
-        $Oversea = County::where('region', 'oversea')->pluck('cn_name')->toArray();
-
         return [
-            'north'=>FollowPlayer::whereIn('area',$northAreas)->get()->count(),
-            'middle'=>FollowPlayer::whereIn('area',$middleAreas)->get()->count(),
-            'south'=>FollowPlayer::whereIn('area',$southAreas)->get()->count(),
-            'east'=>FollowPlayer::whereIn('area',$EastAreas)->get()->count(),
-            'out_island'=>FollowPlayer::whereIn('area',$OutIslandAreas)->get()->count(),
-            'oversea'=>FollowPlayer::whereIn('area',$Oversea)->get()->count()
+            'north'=>FollowPlayer::where('area', 'LIKE', '%'.'north'.'%')->get()->count(),
+            'middle'=>FollowPlayer::where('area', 'LIKE', '%'.'middle'.'%')->get()->count(),
+            'south'=>FollowPlayer::where('area', 'LIKE', '%'.'south'.'%')->get()->count(),
+            'east'=>FollowPlayer::where('area', 'LIKE', '%'.'east'.'%')->get()->count(),
+            'out_island'=>FollowPlayer::where('area', 'LIKE', '%'.'out_island'.'%')->get()->count(),
+            'oversea'=>FollowPlayer::where('area', 'LIKE', '%'.'oversea'.'%')->get()->count()
         ];
 
     }
