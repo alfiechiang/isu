0c6fbbf803dc4737c2d085d10b384c906816d363
zack <zack_apollo_t0817@ie8.com>
2023-11-16 11:20:03 +0800

    update

--- app/Services/Customers/FollowPlayerService.php
+++ app/Services/Customers/FollowPlayerService.php
@@ -36,10 +36,10 @@ class FollowPlayerService
         
 
         if( $total_count-$res->count()>0){
-            // $early_time=FollowPlayer::where('review',1)->orderBy('updated_at')->limit(1)->first()->updated_at;
-            // $residue_count=$total_count-$res->count();
-            // $res2=FollowPlayer::where('review',1)->where('updated_at','>=',$early_time)->limit($residue_count)->get();
-            // $res = $res2->merge($res);
+            $early_time=FollowPlayer::where('review',1)->orderBy('updated_at')->limit(1)->first()->updated_at;
+            $residue_count=$total_count-$res->count();
+            $res2=FollowPlayer::where('review',1)->where('updated_at','>=',$early_time)->limit($residue_count)->get();
+            $res = $res2->merge($res);
         }
 
         return $res;
