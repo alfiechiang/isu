a1fd3ad831741ccab01d59e2e2d4adbe1d8f619f
zack <zack_apollo_t0817@ie8.com>
2023-12-19 09:42:16 +0700

    優惠卷bug

--- app/Services/Stores/ExchangeCouponService.php
+++ app/Services/Stores/ExchangeCouponService.php
@@ -7,6 +7,7 @@ use App\Models\CustomCouponCustomer;
 use App\Models\CouponCustomer;
 use App\Models\CustomCoupon;
 use App\Models\Customer;
+use Illuminate\Support\Facades\Auth;
 
 class ExchangeCouponService
 {
@@ -15,11 +16,14 @@ class ExchangeCouponService
     public function exchange($data){
 
         $coupon1 = Coupon::where("id", $data['coupon_code'])->first();
+        $now =date('Y-m-d H:i:s');
+        $store_name=Auth::user()->name;
         if (!is_null($coupon1)) {
             $customer = Customer::where('guid', $data['guid'])->first();
             CouponCustomer::where('id', $data['id'])
                 ->where('customer_id', $customer->id)
-                ->where('coupon_id', $data['coupon_code'])->update(['status' => 2]);
+                ->where('coupon_id', $data['coupon_code'])->update(
+                    ['status' => 2,'consumed_at'=>$now,'exchange_store'=>$store_name]);
         }
 
         $coupon2 = CustomCoupon::where("code", $data['coupon_code'])->first();
@@ -27,8 +31,9 @@ class ExchangeCouponService
             CustomCouponCustomer::where('id', $data['id'])
                 ->where('coupon_code', $data['coupon_code'])
                 ->where('guid', $data['guid'])
-                ->update(['exchange' => 1]);
+                ->update(['exchange' => 1,'exchange_time'=>$now,'exchange_place'=>$store_name]);
         }
+
     }
 
 }
